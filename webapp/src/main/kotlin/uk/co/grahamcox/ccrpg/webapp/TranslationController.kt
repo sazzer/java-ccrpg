0fd7b429430c7190aea165e3b97efb966ed88d1e
Graham Cox <graham@grahamcox.co.uk>
2014-08-12 14:10:45 +0100

    Controller to load translations actually loads them from the Spring message source now, so all translations can be written in one place

--- webapp/src/main/kotlin/uk/co/grahamcox/ccrpg/webapp/TranslationController.kt
+++ webapp/src/main/kotlin/uk/co/grahamcox/ccrpg/webapp/TranslationController.kt
@@ -8,13 +8,16 @@ import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.context.support.ExposedMessageSource
+import java.util.Locale
+import java.util.HashMap
 
 /**
  * Controller to load the translations
  */
 [Controller]
 [RequestMapping(value = array("/locales/translation"))]
-class TranslationController {
+class TranslationController(private val messageSource: ExposedMessageSource) {
     /**
      * Actually load the requested translations
      * @param locale The locale to load
@@ -22,7 +25,29 @@ class TranslationController {
     [RequestMapping(value = array("/{locale}"))]
     [ResponseBody]
     fun loadTranslations([PathVariable(value = "locale")] locale: String) : Map<String, Any> {
-        val result = mapOf("hello" to locale, "world" to mapOf("earth" to "home", "mars" to "away"))
+        val result = hashMapOf<String, Any>()
+
+        messageSource.getAllMessages(Locale.forLanguageTag(locale)).forEach { it ->
+            val key = it.key.toString().split("\\.")
+            val fieldName = key.last()
+            key.take(key.size - 1).fold(result) { a, b ->
+                var next = hashMapOf<String, Any>()
+                var current = a.get(b)
+
+                if (current != null) {
+                    if (current is HashMap<*, *>) {
+                        next = current as HashMap<String, Any>
+                    } else {
+                        throw IllegalStateException("Message prefix exists as a message key: ${it}")
+                    }
+                } else {
+                    a.put(b, next)
+                }
+
+                next
+            }.put(fieldName, it.value)
+        }
+
         return result
     }
 }
