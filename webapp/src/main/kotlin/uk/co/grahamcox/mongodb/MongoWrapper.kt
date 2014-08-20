74deaa84ef2a071e82bc03db7f56f035ef3816ec
Graham Cox <graham@grahamcox.co.uk>
2014-08-20 20:35:05 +0100

    Start up an embedded MongoDB server when running in dev or test mode

---
+++ webapp/src/main/kotlin/uk/co/grahamcox/mongodb/MongoWrapper.kt
@@ -0,0 +1,37 @@
+package uk.co.grahamcox.mongodb
+
+import de.flapdoodle.embed.mongo.MongodStarter
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder
+import de.flapdoodle.embed.mongo.distribution.Version
+import de.flapdoodle.embed.mongo.config.Net
+import de.flapdoodle.embed.process.runtime.Network
+import de.flapdoodle.embed.mongo.MongodProcess
+
+/**
+ * Bean to start and stop the embedded MongoDB server
+ */
+class MongoWrapper {
+    /** The MongoDB process */
+    var mongodProcess: MongodProcess? = null
+
+    /**
+     * Start MongoDB
+     */
+    fun start() {
+        val starter = MongodStarter.getDefaultInstance()
+        val config = MongodConfigBuilder()
+            .version(Version.Main.PRODUCTION)
+            ?.net(Net(12345, Network.localhostIsIPv6()))
+            ?.build();
+
+        val mongodExecutable = starter?.prepare(config)
+        mongodProcess = mongodExecutable?.start()
+    }
+
+    /**
+     * Stop MongoDB
+     */
+    fun stop() {
+        mongodProcess?.stop()
+    }
+}
