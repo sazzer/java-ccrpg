a3241ac64ca75c692cafe415d65a42206076cfc6
Graham Cox <graham@grahamcox.co.uk>
2014-08-21 13:17:28 +0100

    Actually load data into the database from files now

--- webapp/src/main/kotlin/uk/co/grahamcox/mongodb/MongoPopulator.kt
+++ webapp/src/main/kotlin/uk/co/grahamcox/mongodb/MongoPopulator.kt
@@ -5,6 +5,8 @@ import org.springframework.core.io.Resource
 import org.slf4j.LoggerFactory
 import com.mongodb.BasicDBObject
 import org.bson.types.ObjectId
+import com.fasterxml.jackson.databind.ObjectMapper
+import java.io.FileNotFoundException
 
 /**
  * Mechanism to populate the given DB with sources from the given map
@@ -35,9 +37,12 @@ class MongoPopulator(val db: DB, val sources: Map<String, Resource>) {
      * @param source The source to load the data from
      */
     private fun loadData(source: Resource) : List<BasicDBObject> {
-        return listOf(BasicDBObject()
-                ?.append("_id", ObjectId())
-                ?.append("name", "Graham")
-                ?: throw IllegalArgumentException())
+        val objectMapper = ObjectMapper()
+        val values = objectMapper.readValue(
+                source.getInputStream() ?: throw FileNotFoundException("File not found: ${source}"),
+                javaClass<List<Map<String, Any>>>())
+                ?: throw IllegalStateException("Failed to load data from: ${source}")
+
+        return values.map { value -> BasicDBObject(value) }
     }
 }
