2d3037170b9100cc858ea5d76d59d055dd67d0d1
Graham Cox <graham@grahamcox.co.uk>
2014-08-21 12:21:03 +0100

    Actually put some data into the database. It's not from anywhere yet though

--- webapp/src/main/kotlin/uk/co/grahamcox/mongodb/MongoPopulator.kt
+++ webapp/src/main/kotlin/uk/co/grahamcox/mongodb/MongoPopulator.kt
@@ -3,6 +3,8 @@ package uk.co.grahamcox.mongodb
 import com.mongodb.DB
 import org.springframework.core.io.Resource
 import org.slf4j.LoggerFactory
+import com.mongodb.BasicDBObject
+import org.bson.types.ObjectId
 
 /**
  * Mechanism to populate the given DB with sources from the given map
@@ -16,8 +18,26 @@ class MongoPopulator(val db: DB, val sources: Map<String, Resource>) {
     }
 
     fun populate() {
-        for ((collection, source) in sources) {
-            LOG.debug("Populating collection {} from source {}", collection, source)
+        for ((collectionName, source) in sources) {
+            LOG.debug("Populating collection {} from source {}", collectionName, source)
+            val collection = db.getCollection(collectionName) ?:
+                    throw IllegalStateException("Failed to get collection ${collectionName}")
+            val data = loadData(source)
+            for (record in data) {
+                LOG.debug("Adding record {} to collection {}", data, collection)
+                collection.insert(data)
+            }
         }
+    }
+
+    /**
+     * Load the data from the provided resource so that it can be stored to the database
+     * @param source The source to load the data from
+     */
+    private fun loadData(source: Resource) : List<BasicDBObject> {
+        return listOf(BasicDBObject()
+                ?.append("_id", ObjectId())
+                ?.append("name", "Graham")
+                ?: throw IllegalArgumentException())
     }
 }
