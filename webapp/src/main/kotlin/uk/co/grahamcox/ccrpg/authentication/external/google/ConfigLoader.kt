064a906d1728795694a0f5403167d8f104462137
Graham Cox <graham@grahamcox.co.uk>
2014-08-25 19:24:04 +0100

    External annotations for MongoDB, plus the start of the Google Auth Mongo DAO

--- webapp/src/main/kotlin/uk/co/grahamcox/ccrpg/authentication/external/google/ConfigLoader.kt
+++ webapp/src/main/kotlin/uk/co/grahamcox/ccrpg/authentication/external/google/ConfigLoader.kt
@@ -1,18 +1,28 @@
 package uk.co.grahamcox.ccrpg.authentication.external.google
 
 import java.net.URI
+import uk.co.grahamcox.ccrpg.dao.NoRecordFoundException
+import org.slf4j.LoggerFactory
 
 /**
  * Loader to load the configuration for the Google Authorization endpoint
  */
 class ConfigLoader(private val dao: GoogleConfigDao) {
+    class object {
+        private val LOG = LoggerFactory.getLogger(javaClass<ConfigLoader>())
+    }
     /**
      * Load the configuration
      * @return the configuration
      */
-    fun loadConfig() : Config? = Config(clientId = "380974699378-8l31mu6eo16gbbhq3ph6i4a6ibg3asrt.apps.googleusercontent.com",
-            clientSecret = "7Ubw0_-WDRxVVZE113LfQp8Z",
-            redirectUri = URI("http://localhost:8080/api/authentication/external/google/callback"),
-            authorizationEndpoint = URI("https://accounts.google.com/o/oauth2/auth"),
-            tokenEndpoint = URI("https://accounts.google.com/o/oauth2/token"))
+    fun loadConfig() : Config? {
+        val result: Config? =
+        try {
+            dao.loadConfig()
+        } catch (e: NoRecordFoundException) {
+            LOG.warn("No configuration found")
+            null
+        }
+        return result
+    }
 }
--- webapp/src/main/kotlin/uk/co/grahamcox/ccrpg/authentication/external/google/mongo/GoogleConfigMongoDao.kt
+++ webapp/src/main/kotlin/uk/co/grahamcox/ccrpg/authentication/external/google/mongo/GoogleConfigMongoDao.kt
@@ -5,6 +5,8 @@ import uk.co.grahamcox.ccrpg.dao.BaseMongoDao
 import uk.co.grahamcox.ccrpg.authentication.external.google.Config
 import uk.co.grahamcox.ccrpg.dao.NoRecordFoundException
 import uk.co.grahamcox.ccrpg.authentication.external.google.GoogleConfigDao
+import com.mongodb.BasicDBObject
+import java.net.URI
 
 /**
  * Implementation of the Google Config DAO that works in terms of the MongoDB store
@@ -16,6 +18,14 @@ class GoogleConfigMongoDao(mongoDb: DB) : BaseMongoDao(mongoDb, "externalAuthent
      * @throws NoRecordFoundException if the record wasn't found
      */
     override fun loadConfig(): Config {
-        throw NoRecordFoundException()
+        val record = collection.findOne(BasicDBObject("provider", "google"))
+        if (record == null) {
+            throw NoRecordFoundException()
+        }
+        return Config(clientId = "380974699378-8l31mu6eo16gbbhq3ph6i4a6ibg3asrt.apps.googleusercontent.com",
+                clientSecret = "7Ubw0_-WDRxVVZE113LfQp8Z",
+                redirectUri = URI("http://localhost:8080/api/authentication/external/google/callback"),
+                authorizationEndpoint = URI("https://accounts.google.com/o/oauth2/auth"),
+                tokenEndpoint = URI("https://accounts.google.com/o/oauth2/token"))
     }
 }
