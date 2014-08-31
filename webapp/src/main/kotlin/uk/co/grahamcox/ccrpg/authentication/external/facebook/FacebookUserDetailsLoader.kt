8e53d2739f6373f5aae43c95f11f3ed58b5fa9e6
Graham Cox <graham@grahamcox.co.uk>
2014-08-31 18:33:49 +0100

    Get the user ID by using the Facebook graph API for the access token profile

--- webapp/src/main/kotlin/uk/co/grahamcox/ccrpg/authentication/external/facebook/FacebookUserDetailsLoader.kt
+++ webapp/src/main/kotlin/uk/co/grahamcox/ccrpg/authentication/external/facebook/FacebookUserDetailsLoader.kt
@@ -8,6 +8,8 @@ import uk.co.grahamcox.ccrpg.authentication.external.oauth2.ConfigLoader
 import kotlin.properties.Delegates
 import org.springframework.web.client.RestTemplate
 import uk.co.grahamcox.ccrpg.TextPlainToMapHttpMessageConverter
+import java.net.URI
+import org.springframework.web.util.UriComponentsBuilder
 
 /**
  * User Details loader to load the details from a Facebook login
@@ -17,12 +19,26 @@ class FacebookUserDetailsLoader : UserDetailsLoader {
         /** The logger to use */
         val LOG = LoggerFactory.getLogger(javaClass<FacebookUserDetailsLoader>())
     }
-    /** The mechanism to get the applications access token */
-    var appAccessTokenLoader: AppAccessTokenLoader by Delegates.notNull()
+    /** The Facebook URI to call to get the user profile about an access token */
+    var userProfileUrl = URI("https://graph.facebook.com/me")
+
+    /** the mechanism by which to make HTTP calls */
+    var restTemplate: RestTemplate = RestTemplate();
 
     /** {@inheritDoc} */
     override fun getUserDetails(accessToken: AccessTokenResponse): AuthenticatedUser {
-        val appAccessToken = appAccessTokenLoader.getAppAccessToken()
-        return AuthenticatedUser(source = "facebook", id = appAccessToken)
+        val uri = UriComponentsBuilder.fromUri(userProfileUrl)
+                ?.queryParam("access_token", accessToken.accessToken)
+                ?.build()
+                ?.toUri()
+                ?: throw IllegalStateException("No URI built")
+
+        LOG.debug("Requesting debug information for access token using: {}", uri)
+        val accessTokenDetails = restTemplate.getForObject(uri,
+                javaClass<Map<String, String>>()) ?: throw IllegalStateException("No response returned")
+        LOG.debug("Debug information for access token: {}", accessTokenDetails)
+
+        return AuthenticatedUser(source = "facebook",
+                id = accessTokenDetails.get("id") ?: throw IllegalStateException("No user ID provided"))
     }
 }
