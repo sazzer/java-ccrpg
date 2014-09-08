4e78baf02c4131728fe5620b3de87fcca97aacdc
Graham Cox <graham@grahamcox.co.uk>
2014-09-08 12:50:22 +0100

    Tests for the TextPlain to Map converter

--- webapp/src/test/kotlin/uk/co/grahamcox/ccrpg/TextPlainToMapHttpMessageConverterTest.kt
+++ webapp/src/test/kotlin/uk/co/grahamcox/ccrpg/TextPlainToMapHttpMessageConverterTest.kt
@@ -3,20 +3,36 @@ package uk.co.grahamcox.ccrpg
 import org.spek.Spek
 import org.springframework.http.MediaType
 import org.junit.Assert
-import org.junit.Ignore
+import org.springframework.mock.http.MockHttpInputMessage
+import java.nio.charset.Charset
 
-[Ignore]
 class TextPlainToMapHttpMessageConverterTest : Spek() {{
     given("The converter") {
         val converter = TextPlainToMapHttpMessageConverter()
+        val supported = javaClass<java.util.Map<String, String>>()
 
         on("Checking what it supports") {
             it("Supports decoding into a Java Map") {
-                val supported = javaClass<java.util.Map<*, *>>()
-                Assert.assertTrue(converter.canRead(supported, MediaType.MULTIPART_FORM_DATA))
+                Assert.assertTrue(converter.canRead(supported, MediaType.TEXT_PLAIN))
             }
             it("Doesn't support decoding into a String") {
-                Assert.assertFalse(converter.canRead(javaClass<String>(), MediaType.MULTIPART_FORM_DATA))
+                Assert.assertFalse(converter.canRead(javaClass<String>(), MediaType.TEXT_PLAIN))
+            }
+        }
+        on("Decoding a valid string") {
+            val inputMessage = MockHttpInputMessage("hello=world&answer=42".toByteArray(Charset.forName("UTF-8")))
+
+            val converted = converter.read(supported, inputMessage)
+                    ?: throw AssertionError("No converted value returned")
+
+            it("Has two entries") {
+                Assert.assertEquals(2, converted.size())
+            }
+            it("Has hello equal to world") {
+                Assert.assertEquals("world", converted.get("hello"))
+            }
+            it("Has answer equal to 42") {
+                Assert.assertEquals("42", converted.get("answer"))
             }
         }
     }
